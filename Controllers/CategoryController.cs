38ed86709a50af78f900457f4cba7bbdf09d7254
Md Abdullah Al Masum <52232643+masum029@users.noreply.github.com>
2024-04-24 16:58:38 +0600

    initial Commit.Category Added.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly InventoryContext _inventoryContext;
+        public CategoryController(InventoryContext inventoryContext)
+        {
+            _inventoryContext = inventoryContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult CategryList()
+        {
+            var data= _inventoryContext.Categories.ToList();
+            return new JsonResult (data);
+        }
+
+    }
+}
---
+++ wwwroot/js/Inventory/Category.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+});
+
+function GetCategory() {
+    $.ajax({
+        url: '/Category/GetCategory',
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json;charset=utf-8',
+        success: function (response) {
+            if (response == null || response == undefined || response.length == 0) {
+
+                var obj = '';
+                obj += '<tr>';
+                obj += '<td class=clspan="2">' + 'Category Not Available.' + '</td>';
+                obj += '</tr>';
+
+                $('#table_data').html(obj);
+            } else {
+                var obj = '';
+                $.each(response, function (index, item) {
+                    obj += '<tr>'
+                    obj += '<td>' + item.CategoryID + '</td>';
+                    obj += '<td>' + item.CategoryName + '</td>';
+                    obj += '<td><a href="#" class="btn btn-sm btn-success" onclick="Edit(' + item.CategoryID + ')">Edit</a>||<a href="#" class="btn btn-sm btn-danger" onclick=Delete(' + item.CategoryID + ')>Delete</a></td>';
+                    obj+='</tr>'
+                })
+
+            }
+        }
+    })
+}
