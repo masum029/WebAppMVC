37c66f6df4f81034562a21659f34b279b41c2399
Md Abdullah Al Masum <52232643+masum029@users.noreply.github.com>
2024-04-25 16:49:19 +0600

    Category Saved using Modal.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMVC.Models;
 
 namespace WebAppMVC.Controllers
 {
@@ -9,16 +10,70 @@ namespace WebAppMVC.Controllers
         {
             _inventoryContext = inventoryContext;
         }
+
         public IActionResult Index()
         {
             return View();
         }
 
+
+
         public JsonResult CategryList()
         {
-            var data= _inventoryContext.Categories.ToList();
-            return new JsonResult (data);
+            var data = _inventoryContext.Categories.ToList();
+            return new JsonResult(data);
         }
 
+
+        public JsonResult Insert(Category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _inventoryContext.Add(category);
+                    _inventoryContext.SaveChanges();
+                    return Json("Category Saved Successfully");
+                }
+                return Json("Modal Validation Failed");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public JsonResult Edit(int id)
+        {
+            var category = _inventoryContext.Categories.FirstOrDefault(x => x.CategoryID == id);
+            return Json(category);
+        }
+
+        [HttpPost]
+        public JsonResult Update(Category category) {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _inventoryContext.Categories.Update(category);
+                    _inventoryContext.SaveChanges();
+                    return Json("Product Details Updated.");
+                }
+                return Json("Update Failed");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        //public JsonResult Delete(int id)
+        //{
+        //    var category = _inventoryContext.Categories.Find(x => x.CategoryID == id);
+        //    return Json(category);
+        //}
     }
 }
--- wwwroot/js/Inventory/Category.js
+++ wwwroot/js/Inventory/Category.js
@@ -2,6 +2,8 @@
     GetCategory();
 });
 
+
+
 function GetCategory() {
     $.ajax({
         url: '/Category/CategryList',
@@ -14,7 +16,7 @@ function GetCategory() {
 
                 var obj = '';
                 obj += '<tr>';
-                obj += '<td class=clspan="3">' + 'Category Not Available.' + '</td>';
+                obj += '<td colspan="3">' + 'Category Not Available.' + '</td>';
                 obj += '</tr>';
 
                 $('#table_data').html(obj);
@@ -25,7 +27,7 @@ function GetCategory() {
                     obj += '<tr>'
                     obj += '<td>' + item.categoryID + '</td>';
                     obj += '<td>' + item.categoryName + '</td>';
-                    obj += '<td><a href="#" class="btn btn-sm btn-success" onclick="Edit(' + item.CategoryID + ')">Edit</a>||<a href="#" class="btn btn-sm btn-danger" onclick=Delete(' + item.CategoryID + ')>Delete</a></td>';
+                    obj += '<td><a href="#" class="btn btn-sm btn-success" onclick="Edit(' + item.CategoryID + ')">Edit</a> | <a href="#" class="btn btn-sm btn-danger" onclick=Delete(' + item.CategoryID + ')>Delete</a></td>';
                     obj += '</tr>'
 
                 });
@@ -33,8 +35,73 @@ function GetCategory() {
             }
         },
         error: function () {
+            debugger
             alert('Unable to read the data.')
         }
 
     })
+}
+
+
+$('#btnAddCategory').click(function () {
+    $('#addCategoryModal').modal('show');
+    $('#addCategoryModalLabel').text('Add Category');
+});
+
+function HideModal() {
+    clearData();
+    $('#addCategoryModal').modal('hide');
+}
+
+function clearData() {
+    $('#CategoryName').val('');
+}
+
+
+function validate() {
+    var isValid = true;
+    if ($('#CategoryName').val().trim() == "") {
+        $('#CategoryName').css('border-color', 'Red');
+        isValid = false;
+    } else {
+        $('#CategoryName').css('border-color', 'lightgrey');
+    }
+
+    return isValid;
+}
+
+$('#CategoryName').change(function () {
+    validate();
+});
+function Insert() {
+    debugger
+
+    var result = validate();
+    if (result == false) {
+        return false;
+    }
+    var fromData = new Object();
+    fromData.CategoryID = $('#CategoryID').val();
+    fromData.categoryName = $('#CategoryName').val();
+
+
+    $.ajax({
+        url: '/category/Insert',
+        data: fromData,
+        type: 'post',
+
+        success: function (response) {
+            if (response == null || response == undefined || response.length == 0) {
+
+                alert("Unable to save data.")
+            } else {
+                HideModal();
+                GetCategory();
+                alert(response);
+            }
+        },
+        error: function () {
+            alert('Unable to save the data');
+        }
+    });
 }
